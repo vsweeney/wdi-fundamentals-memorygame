f3c05fea7cf2821b74247c7e328400b4cd1b333d
Vincent L Sweeney Jr <vincentswee@gmail.COM>
2017-06-21 16:29:14 -0400

    created game logic

---
+++ main.js
@@ -0,0 +1,23 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+ if (cardsInPlay [0] === cardsInPlay [1]) {
+		console.log("You've found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
+
+	var flipCard = function (cardId) {
+		console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+
+	checkForMatch();
+ }
+};
+flipCard(0);
+flipCard(2);
