79d2e5afb34579a9febf4f685df81b17e4407d42
Vincent L Sweeney Jr <vincentswee@gmail.COM>
2017-06-22 17:41:04 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-var cards = ["queen", "queen", "king", "king"];
+//Creating new arrays to include four objects, one for each card
+var cards = [
+{
+	rank: 'queen',
+	suit: 'harts',
+	cardImage: 'images/queen-of-harts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'harts',
+	cardImage: 'images/king-of-harts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
+// Cards in play array
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -10,14 +33,21 @@ var checkForMatch = function () {
 };
 
 	var flipCard = function (cardId) {
-		console.log("User flipped " + cards[cardId]);
+		console.log("User flipped " + cards[cardId].rank);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].card);
+
+console.log(cards[cardId].cardImage);
+
+console.log(cards[cardId].suit);
+
 
 if (cardsInPlay.length === 2) {
 
 	checkForMatch();
+	cardsInPlay = [];
  }
 };
+
 flipCard(0);
 flipCard(2);
